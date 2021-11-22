ffa777e84bc06ea1d0feeb8135dc98896a9723df
Theofahey <tfahey20@stuy.edu>
2021-11-22 11:47:59 -0500

    added name and directory in shell line

--- shell.c
+++ shell.c
@@ -2,7 +2,13 @@
 
 int main() {
     while (1) {
-        printf("catsh$ ");
+        char path[1000];
+        getcwd(path,1000);
+        char ** code;
+        int n;
+        n = countDelimiters(path,'/');
+        code = parse_args(path, '/');
+        printf("%s %s catsh$ ", getlogin(),code[n-2]);
         fflush(stdout);
         char command[1000] = {0};
         read(STDIN_FILENO, command, 100);
