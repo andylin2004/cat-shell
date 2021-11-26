398edd3ddc2ff906dcf3dcbe08818ea0c0587d59
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-11-25 21:56:34 -0500

    revert back to last version with functioning piping

--- execute.c
+++ execute.c
@@ -4,13 +4,12 @@ void cd(char **args);
 char **redirectionParseAndSetup(char **input);
 void popenTest();
 void executeCommand(char **commands, int pipes);
-void executeCommandFork(char **commands, int start, int end, int pipeNum);
+void executeCommandFork(char **commands, int start, int end);
 int standardOutReal;
 int standardInReal;
-int standardOutTemp;
-int standardInTemp;
 int status;
 int *pipefd;
+int pipeNum = 0;
 int pipes;
 
 void executeLine(char *input)
@@ -26,45 +25,34 @@ void executeLine(char *input)
         redirect = countDelimiters(commands[i], '<') + countDelimiters(commands[i], '>') - 2;
         pipes = countDelimiters(commands[i], '|') - 1;
         args = parse_args(commands[i], ' ');
-        if (strcmp(args[0], "cd") == 0)
+        if (fork())
         {
-            cd(args);
+            wait(&status);
         }
-        else if (strcmp(args[0], "exit") == 0)
+        else
         {
-            kill(getppid(), SIGTERM); //ppid is the shell
-        }
-        if (redirect || pipes)
-        {
-            standardOutReal = dup(STDOUT_FILENO);
-            standardInReal = dup(STDIN_FILENO);
+            if (strcmp(args[0], "cd") == 0)
+            {
+                cd(args);
+            }
+            else if (strcmp(args[0], "exit") == 0)
+            {
+                kill(getppid(), SIGTERM); //ppid is the shell
+            }
             if (redirect)
             {
+                standardOutReal = dup(STDOUT_FILENO);
+                standardInReal = dup(STDIN_FILENO);
                 args = redirectionParseAndSetup(args);
             }
-        }
-        executeCommand(args, pipes);
-        wait(&status);
-        if (redirect || pipes)
-        {
-            if (pipes)
+            executeCommand(args, pipes);
+            // execvp(args[0], args);
+            if (redirect || pipes)
             {
-                for (i = 0; i < pipes * 2; i++)
-                {
-                    close(pipefd[i]);
-                }
-            }
-            dup2(standardInReal, STDIN_FILENO);
-            dup2(standardOutReal, STDOUT_FILENO);
-            close(standardInReal);
-            close(standardOutReal);
-            if (standardInTemp)
-            {
-                close(standardInTemp);
-            }
-            if (standardOutTemp)
-            {
-                close(standardOutTemp);
+                dup2(standardInReal, STDIN_FILENO);
+                dup2(standardOutReal, STDOUT_FILENO);
+                close(standardInReal);
+                close(standardOutReal);
             }
         }
     }
@@ -75,36 +63,35 @@ void executeCommand(char **commands, int pipes) //this will deal with pipings
     char **args;
     pipefd = malloc(sizeof(int) * pipes);
     int i;
-    for (i = 0; i < pipes; i++)
-    {
+    for (i = 0; i < pipes; i++){
         pipe(pipefd + i * 2);
     }
 
-    executeCommandFork(commands, 0, 0, 0);
+    executeCommandFork(commands, 0, 0);
 
-    for (i = 0; i < pipes; i++)
-    {
+    for (i = 0; i < pipes; i++){
         wait(&status);
     }
+    for (i = 0; i < pipes * 2; i++){
+            close(pipefd[i]);
+        }
+
+    pipeNum = 0;
 }
 
-void executeCommandFork(char **commands, int start, int end, int pipeNum)
-{
+void executeCommandFork(char **commands, int start, int end){
     char **args;
     int i;
-    for (; end < lengthOfArray(commands) && *commands[end] != '|'; end++)
-    {
-        printf("%s\n", commands[end]);
-    }
+    for (; end < lengthOfArray(commands) && *commands[end] != '|'; end++);
     int newStart = end + 1;
     end--;
     args = malloc(end - start + 1);
     args[end - start] = NULL;
     for (; end >= start; end--)
     {
-        printf("%s\n", commands[end]);
         args[end - start] = commands[end];
     }
+    dup2(standardOutReal, STDOUT_FILENO);
     if (fork() == 0)
     {
         printf("%d\n", pipeNum);
@@ -112,34 +99,19 @@ void executeCommandFork(char **commands, int start, int end, int pipeNum)
         {
             dup2(pipefd[pipeNum + 1], STDOUT_FILENO);
         }
-        else if (standardOutTemp)
-        {
-            dup2(standardOutTemp, STDOUT_FILENO);
-        }
 
-        if (pipeNum - 2 >= 0)
-        {
+        if (pipeNum - 2 >= 0){
             dup2(pipefd[pipeNum - 2], STDIN_FILENO);
         }
-        else if (standardInTemp)
-        {
-            printf("head");
-            dup2(standardInTemp, STDIN_FILENO);
-        }
-
-        for (i = 0; i < pipes * 2; i++)
-        {
+        
+        for (i = 0; i < pipes * 2; i++){
             close(pipefd[i]);
         }
-
         execvp(args[0], args);
-    }
-    else
-    {
+    }else{
         pipeNum += 2;
-        if (pipeNum <= pipes * 2)
-        {
-            executeCommandFork(commands, newStart, newStart, pipeNum);
+        if (pipeNum <= pipes * 2){
+            executeCommandFork(commands, newStart, newStart);
         }
     }
 }
@@ -147,6 +119,8 @@ void executeCommandFork(char **commands, int start, int end, int pipeNum)
 char **redirectionParseAndSetup(char **input)
 {
     char **current = input;
+    int stdoutFile;
+    int stdinFile;
     char **newInput = malloc(sizeof(input));
     int i = 0;
 
@@ -157,18 +131,18 @@ char **redirectionParseAndSetup(char **input)
             if (*(*current + 1) == '>')
             {
                 current++;
-                standardOutTemp = open(*current, O_WRONLY | O_CREAT | O_APPEND, 0777);
+                stdoutFile = open(*current, O_WRONLY | O_CREAT | O_APPEND, 0777);
             }
             else
             {
                 current++;
-                standardOutTemp = open(*current, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+                stdoutFile = open(*current, O_WRONLY | O_CREAT | O_TRUNC, 0777);
             }
         }
         else if (**current == '<')
         {
             current++;
-            standardInTemp = open(*current, O_RDONLY, 0777);
+            stdinFile = open(*current, O_RDONLY, 0777);
         }
         else
         {
@@ -178,13 +152,15 @@ char **redirectionParseAndSetup(char **input)
         current++;
     }
     newInput[i] = NULL;
-    if (standardOutTemp)
+    if (stdoutFile)
     {
-        dup2(standardOutTemp, STDOUT_FILENO);
+        dup2(stdoutFile, STDOUT_FILENO);
+        close(stdoutFile);
     }
-    if (standardInTemp)
+    if (stdinFile)
     {
-        dup2(standardInTemp, STDIN_FILENO);
+        dup2(stdinFile, STDIN_FILENO);
+        close(stdinFile);
     }
     return newInput;
 }
