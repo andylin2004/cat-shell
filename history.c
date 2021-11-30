a51e6ec590abbdb2d183dcfbdf8a7a8812ef5604
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-11-29 22:13:36 -0500

    error handling for no commands in history

--- history.c
+++ history.c
@@ -23,17 +23,21 @@ void readHistory(){
     int fileSize = fileInfo.st_size;
     char *historyFileContents = malloc(fileSize + sizeof(char));
     read(historyFile, historyFileContents, fileSize + sizeof(char));
-    historyLength = countDelimiters(historyFileContents, '\n');
+    historyLength = countDelimiters(historyFileContents, '\n') - 1;
     history = parse_args(historyFileContents, '\n');
 }
 
 void printHistory(){
     int i;
     readHistory();
-    printf("History of commands used: \n");
-    for (i = 0; i < historyLength - 2; i++)
-    {
-        printf("%d: %s\n", i, history[i]);
+    if (historyLength - 1){
+        printf("History of commands used: \n");
+        for (i = 0; i < historyLength - 1; i++)
+        {
+            printf("%d: %s\n", i, history[i]);
+        }
+    }else{
+        printf("No commands in history.\n");
     }
 }
 
