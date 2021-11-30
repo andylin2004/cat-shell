97954178ae4c9866d5266f8b49c68872d986faa2
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-11-29 20:37:12 -0500

    fix bug where cwd was mislabeled as null

--- shell.c
+++ shell.c
@@ -9,6 +9,9 @@ int main() {
         int n;
         n = countDelimiters(path,'/');
         code = parse_args(path, '/');
+        if (!code[n-2]){
+            code[n-2] = "/"; // only time this is null is at root directory
+        }
         printf("%s %s catsh$ ", getlogin(),code[n-2]);
         fflush(stdout);
         char command[1000] = {0};
